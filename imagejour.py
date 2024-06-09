84ae0cb24657a1d8f0ca09ddd67add3dd8c8fb0e
Philippeti <70230760+Philippeti@users.noreply.github.com>
2024-06-09 17:44:58 +0100

    Create imagejour.py

---
+++ imagejour.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+def importima(url):
+    extension = url.split('.')[-1].lower()
+    name = url.split('/')[-1]
+    image_extensions = ['jpg', 'jpeg', 'png', 'gif']
+    video_extensions = ['mp4', 'avi', 'mov', 'wmv']
+    if extension in image_extensions:
+        img_response = requests.get(url)
+        with open(name, 'wb') as img_file:
+                img_file.write(img_response.content)
+        return img_response.content
+    elif extension in video_extensions:
+        video_response = requests.get(url)
+        with open(name, 'wb') as video_file:
+                video_file.write(video_response.content)
+        return video_response.content
+        
